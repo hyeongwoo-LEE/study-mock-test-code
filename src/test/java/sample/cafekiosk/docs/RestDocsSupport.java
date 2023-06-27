1e240939282b67c1d1a9ae06b7d7b5e09caa25b6
browoo <leehw@gabia.com>
2023-06-27 18:30:31 +0900

    Section 8

---
+++ src/test/java/sample/cafekiosk/docs/RestDocsSupport.java
@@ -0,0 +1,28 @@
+package sample.cafekiosk.docs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
+import org.springframework.restdocs.mockmvc.MockMvcRestDocumentationConfigurer;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@ExtendWith(RestDocumentationExtension.class)
+public abstract class RestDocsSupport {
+
+    protected MockMvc mockMvc;
+    protected ObjectMapper objectMapper = new ObjectMapper();
+
+    @BeforeEach
+    void setUp(RestDocumentationContextProvider provider) {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(initController())
+                .apply(MockMvcRestDocumentation.documentationConfiguration(provider))
+                .build();
+    }
+
+    protected abstract Object initController();
+}
--- src/test/java/sample/cafekiosk/spring/api/service/product/ProductNumberFactoryTest.java
+++ src/test/java/sample/cafekiosk/spring/api/service/product/ProductNumberFactoryTest.java
@@ -1,20 +1,8 @@
 package sample.cafekiosk.spring.api.service.product;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductNumberFactoryTest {
-    
-    @DisplayName("")
-    @Test
-    public void  () {
-        // given
-        
-        // when
-        
-        // then
-    }
+
 
 }
